da77753946cdea04549452d58450b1bd655172fa
jhoncy <452568102@qq.com>
2016-09-06 09:41:28 +0800

    register update

--- controller/web.js
+++ controller/web.js
@@ -1,3 +1,6 @@
+var models = require('../model/user.js');
+var crypto = require('crypto');
+
 module.exports = {
     register: async (ctx, next) => {
         await ctx.render('web/register', {
@@ -5,14 +8,23 @@ module.exports = {
         });
     },
     UserRegister: async (ctx, next) => {
-        /*try {
+        try {
             var userInfo = {
                 username : ctx.request.body.username,
-                password : ctx.request.body.userpassword,
+                password : ctx.request.body.password,
+            }
+            var md5 = crypto.createHash('md5');
+            userInfo.salt=new Date()+userInfo.username;//md5 salt
+            userInfo.password=md5.update(userInfo.password+userInfo.salt,'utf8').digest('base64');//md5加盐加密
+            var checkUserName = await models.User.find({username:userInfo.username});
+            if (checkUserName.length == 0 ) {
+              await models.User.create(userInfo);
+              ctx.body = "注册成功！";
+            }else {
+              ctx.body = "此用户名已被占用！";
             }
         }catch (e) {
-
-        }*/
-        ctx.body = "into UserRegister!";
+            ctx.body = "注册失败，系统错误";
+        }
     }
 }
--- model/user.js
+++ model/user.js
@@ -1,7 +1,20 @@
-/**
+/**jhoncy
  * Created by jhoncy on 2016/9/2.
  */
 /*
 *   TTD
 **/
+var mongoose = require('mongoose');
 
+// 用户模式
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    salt:String,//md5 salt
+});
+
+// 绑定模型
+var User = mongoose.model('User', userSchema);
+
+// 导出模型
+exports.User = User;
